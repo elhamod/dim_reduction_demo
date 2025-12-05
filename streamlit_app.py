f4147ce4bf10274bc437789abedd5fcb808fd57d
elhamod <55500282+elhamod@users.noreply.github.com>
2025-12-05 10:51:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -530,7 +530,7 @@ def train_pythae_vae(
         no_cuda=(device == "cpu"),
         keep_best_on_train=True,
         scheduler_cls="ReduceLROnPlateau",
-        scheduler_params={"patience": 500, "factor": 0.9},
+        scheduler_params={"patience": 2000, "factor": 0.9},
         # scheduler_cls="MultiStepLR",
         # scheduler_params={
         #     "milestones": [200, 350, 500, 750, 1000],
