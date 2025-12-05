8b0bb72ef37539cf7fd3e08df6522843ebbf3c83
elhamod <55500282+elhamod@users.noreply.github.com>
2025-12-05 01:18:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -318,16 +318,121 @@ def make_3d_pca_geometry(
 # ============================================================
 # Pythae VAE utilities
 # ============================================================
+from typing import List
+import torch.nn as nn
+from pythae.models.nn import BaseDecoder, BaseEncoder
+from pythae.model.base.base_utils import ModelOutput
 
-import numpy as np
-import torch
+layer_1_size = 3
+class Encoder_AE_MLP(BaseEncoder):
+    def __init__(self, args: dict):
+        BaseEncoder.__init__(self)
+        self.input_dim = args.input_dim
+        self.latent_dim = args.latent_dim
+    
+        layers = nn.ModuleList()
 
-from pythae.trainers import BaseTrainerConfig
-from pythae.pipelines import TrainingPipeline
-from torch.utils.data import DataLoader, TensorDataset
-import torch.nn.functional as F
+        layers.append(nn.Sequential(nn.Linear(np.prod(args.input_dim), layer_1_size), nn.ReLU()))
 
-from pythae.trainers.training_callbacks import TrainingCallback
+        self.layers = layers
+        self.depth = len(layers)
+
+        self.embedding = nn.Linear(layer_1_size, self.latent_dim)
+
+    def forward(self, x, output_layer_levels: List[int] = None):
+        output = ModelOutput()
+
+        max_depth = self.depth
+
+        out = x.reshape(-1, np.prod(self.input_dim))
+
+        for i in range(max_depth):
+            out = self.layers[i](out)
+
+            if output_layer_levels is not None:
+                if i + 1 in output_layer_levels:
+                    output[f"embedding_layer_{i+1}"] = out
+            if i + 1 == self.depth:
+                output["embedding"] = self.embedding(out)
+
+        return output
+
+
+
+
+class Encoder_VAE_MLP(BaseEncoder):
+    def __init__(self, args: dict):
+        BaseEncoder.__init__(self)
+        self.input_dim = args.input_dim
+        self.latent_dim = args.latent_dim
+
+        layers = nn.ModuleList()
+
+        layers.append(nn.Sequential(nn.Linear(np.prod(args.input_dim), layer_1_size), nn.ReLU()))
+
+        self.layers = layers
+        self.depth = len(layers)
+
+        self.embedding = nn.Linear(layer_1_size, self.latent_dim)
+        self.log_var = nn.Linear(layer_1_size, self.latent_dim)
+
+    def forward(self, x, output_layer_levels: List[int] = None):
+        output = ModelOutput()
+
+        max_depth = self.depth
+
+        out = x.reshape(-1, np.prod(self.input_dim))
+
+        for i in range(max_depth):
+            out = self.layers[i](out)
+
+            if output_layer_levels is not None:
+                if i + 1 in output_layer_levels:
+                    output[f"embedding_layer_{i+1}"] = out
+            if i + 1 == self.depth:
+                output["embedding"] = self.embedding(out)
+                output["log_covariance"] = self.log_var(out)
+
+        return output
+
+
+
+class Decoder_AE_MLP(BaseDecoder):
+    def __init__(self, args: dict):
+        BaseDecoder.__init__(self)
+
+        self.input_dim = args.input_dim
+
+        layers = nn.ModuleList()
+
+        layers.append(nn.Sequential(nn.Linear(args.latent_dim, layer_1_size), nn.ReLU()))
+
+        layers.append(
+           nn.Linear(layer_1_size, int(np.prod(args.input_dim)))
+        )
+
+        self.layers = layers
+        self.depth = len(layers)
+
+    def forward(self, z: torch.Tensor, output_layer_levels: List[int] = None):
+        output = ModelOutput()
+
+        max_depth = self.depth
+        
+        out = z
+
+        for i in range(max_depth):
+            out = self.layers[i](out)
+
+            if output_layer_levels is not None:
+                if i + 1 in output_layer_levels:
+                    output[f"reconstruction_layer_{i+1}"] = out
+            if i + 1 == self.depth:
+                output["reconstruction"] = out.reshape((z.shape[0],) + self.input_dim)
+
+        return output
+
+
 
 class StreamlitLossCallback(TrainingCallback):
     """
@@ -393,7 +498,7 @@ def train_pythae_vae(
         input_dim=(n_features,),   # 1D vector input
         latent_dim=latent_dim
     )
-    model = VAE(model_config).to(device)
+    model = VAE(model_config, encoder=Encoder_VAE_MLP, decoder=Decoder_AE_MLP).to(device)
     # model_config = AEConfig(
     #     input_dim=(n_features,),   # 1D vector input
     #     latent_dim=latent_dim
