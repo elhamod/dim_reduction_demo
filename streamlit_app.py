dc6e00fcde0770222d5d77a34ba12e321df95984
elhamod <55500282+elhamod@users.noreply.github.com>
2025-12-22 22:29:41 -0500

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -644,7 +644,7 @@ def make_vae_latent_and_manifold_figures(
     fig_manifold = go.Figure()
 
     # Original points (same color as PCA)
-    print("X3_orig", X3_orig)
+    st.warning("X3_orig" + X3_orig)
     fig_manifold.add_trace(
         go.Scatter3d(
             x=X3_orig[:, 0],
@@ -665,7 +665,7 @@ def make_vae_latent_and_manifold_figures(
     )
 
     # VAE recon points (same color as PCA recon)    
-    print("X3_recon", X3_recon)
+    st.warning("X3_recon" + X3_recon)
     fig_manifold.add_trace(
         go.Scatter3d(
             x=X3_recon[:, 0],
@@ -749,14 +749,14 @@ def make_vae_latent_and_manifold_figures(
 
         with torch.no_grad():
             z_tensor = torch.tensor(grid_z.astype(np.float32)).to(device)
-            print("z_tensor", z_tensor)
-            print("Z",Z)
+            st.warning("z_tensor" + z_tensor)
+            st.warning("Z" + Z)
             decoded_out = model.decoder(z_tensor)
             decoded = decoded_out["reconstruction"].cpu().numpy()
 
         decoded3 = decoded[:, idxs].reshape(grid_points, grid_points, 3)
 
-        print("decoded3", decoded3)
+        st.warning("decoded3" + decoded3)
 
         fig_manifold.add_trace(
             go.Surface(
